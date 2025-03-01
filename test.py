5e07039a7feb79f5608e9c75db22e1543da6ce68
nebugg <antonbegun@icloud.com>
2025-03-01 21:44:04 +0300

    added logins and passwords

--- test.py
+++ test.py
@@ -2,76 +2,82 @@ import sqlite3
 
 class BD:
     def __init__(self):            #просто инит
-        con = sqlite3.connect("teachers.sqlite")
-        self.cursor = con.cursor()
+        global coni
+        global cont
+        global cursort
+        global cursori
 
 
     def insert(self, i, n, c, s, a, desc, w, e, p):  #функция, вносящая нового учителя в БД (иднекс, имя, направление, предмет, опыт работы, описание)
-        self.cursor.execute(f"INSERT INTO teachers(ind,name,category,subject,age,description,workplace,education,password) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', c, '"'])}, {''.join(['"', s, '"'])}, {a}, {''.join(['"', desc, '"'])}, {''.join(['"', w, '"'])}, {''.join(['"', e, '"'])}, {''.join(['"', p, '"'])})")
+        cursort.execute(f"INSERT INTO teachers(ind,name,category,subject,age,description,workplace,education,password) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', c, '"'])}, {''.join(['"', s, '"'])}, {a}, {''.join(['"', desc, '"'])}, {''.join(['"', w, '"'])}, {''.join(['"', e, '"'])}, {''.join(['"', p, '"'])})")
+
 
 
     def fetchbyindex(self, i):       #находит препода по индексу (они по порядку будут)
-        res = self.cursor.execute(f"SELECT * FROM teachers WHERE ind = {i}")
+        res = cursort.execute(f"SELECT * FROM teachers WHERE ind = {i}")
         return list(res)
 
 
     def fetchbyname(self, n):         #находит нужных учителей по любому отрывку имени
-        res = self.cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + n + '%',))
+        res = cursort.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + n + '%',))
         return list(res)
 
 
     def fetchbyworkplace(self, w):         #находит нужных учителей по месту работы
-        res = self.cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + w + '%',))
+        res = cursort.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + w + '%',))
         return list(res)
 
 
     def fetchbyeducation(self, e):         #находит нужных учителей по его образованию
-        res = self.cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + e + '%',))
+        res = cursort.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + e + '%',))
         return list(res)
 
 
     def fetchbysubject(self, s):       #находит нужных учителей по любой части названия предмета
-        res = self.cursor.execute("SELECT * FROM teachers WHERE subject like ?", ('%' + s + '%',))
+        res = cursort.execute("SELECT * FROM teachers WHERE subject like ?", ('%' + s + '%',))
         return list(res)
 
 
     def fetchbyage(self, a):    #находит нужных учителей по опыту работы (если опыт больше или равен нужному, выводит страничку препода)
-        res = self.cursor.execute(f"SELECT * FROM teachers WHERE experience >= {a}")
+        res = cursort.execute(f"SELECT * FROM teachers WHERE experience >= {a}")
         return list(res)
 
 
     def fetchbydescription(self, desc):    #находит учителей по любому отрывку описания
-        res = self.cursor.execute("""SELECT * FROM teachers WHERE description like ? """, ('%' + desc + '%',))
+        res = cursort.execute("""SELECT * FROM teachers WHERE description like ? """, ('%' + desc + '%',))
         return list(res)
 
 
     def fetchbycategory(self, cat):    #находит учителей по любому отрывку направления
-        res = self.cursor.execute("""SELECT * FROM teachers WHERE category like ? """, ('%' + cat + '%',))
+        res = cursort.execute("""SELECT * FROM teachers WHERE category like ? """, ('%' + cat + '%',))
         return list(res)
 
 
 class Account:
     def __init__(self, login, password):
-        coni = sqlite3.connect("ids.sqlite")
-        cont = sqlite3.connect("teachers.sqlite")
-        self.cursor = coni.cursor()
-        self.cursort = cont.cursor()
+        global coni
+        global cont
+        global cursort
+        global cursori
         self.login = login
         self.password = password
-        self.cursor.execute(f"INSERT INTO ids(login, password) VALUES({''.join(['"', self.login, '"'])}, {''.join(['"', self.password, '"'])})")
+        cursori.execute(f"INSERT INTO ids(login, password) VALUES({'"' + login + '"'}, {''.join(['"', password, '"'])})")
 
 
     def log_in(self):
-        res = self.cursort.execute(f"""SELECT * FROM teachers WHERE name = {''.join(['"', self.login, '"'])} and password = {''.join(['"', self.password, '"'])} """).fetchall()
+        res = cursort.execute(f"""SELECT * FROM teachers WHERE name = {'"' + self.login + '"'} and password = {''.join(['"', self.password, '"'])} """).fetchall()
         return res
 
 
-
+cont = sqlite3.connect("teachers.sqlite")
+cursort = cont.cursor()
+coni = sqlite3.connect("ids.sqlite")
+cursori = coni.cursor()
 base = BD() #класс базы данных, ему можно дать любое имя, я выбрал base
-base.insert(1, "Anya", "physics and mathematics", "maths", 23, "hello, my friends!", "", "", "b")
-Anya = Account("Anya", "b")
+base.insert(1, "Anya", "physics and mathematics", "maths", 23, "hello, my friends!", "wa", "adsa", "ble")
+base.insert(2, "Tobey", "Internet Technology", "maths", 12, "yo bozo", "", "", "aaa")
+Anya = Account("Anya", "ble")
 print(Anya.log_in())
-#base.insert(2, "Tobey", "Internet Technology", "maths", 12, "yo bozo")
 #print(base.fetchbycategory("in"))
 #print(base.fetchbyindex(1))
 #print(base.fetchbydescription(""))
