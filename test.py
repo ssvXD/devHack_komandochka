b27506ec19ad6e3b58f8ecd03764b574837fd93c
nebugg <antonbegun@icloud.com>
2025-03-01 10:38:21 +0300

    test02

--- test.py
+++ test.py
@@ -1,44 +1,42 @@
 import sqlite3
 
-
-def insert(i, n, s, e, desc):  #функция, вносящая нового учителя в БД (имя, предмет, опыт работы, описание)
-    global cursor
-    cursor.execute(f"INSERT INTO teachers(ind,name,subject,experience,description) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', s, '"'])}, {e}, {''.join(['"', desc, '"'])})")
-
-def fetchbyindex(i):       #находит препода по индексу (они по порядку будут)
-    global cursor
-    res = cursor.execute(f"SELECT * FROM teachers WHERE ind = {i}")
-    return list(res)
-
-def fetchbyname(n):         #находит нужных учителей по любому отрывку имени
-    global cursor
-    res = cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + n + '%',))
-    return list(res)
-
-def fetchbysubject(s):       #находит нужных учителей по любой части названия предмета
-    global cursor
-    res = cursor.execute("SELECT * FROM teachers WHERE subject like ?", ('%' + s + '%',))
-    return list(res)
-
-def fetchbyexperience(e):    #находит нужных учителей по опыту работы (если опыт больше или равен нужному, выводит страничку препода)
-    global cursor
-    res = cursor.execute(f"SELECT * FROM teachers WHERE experience >= {e}")
-    return list(res)
+class BD:
+    def __init__(self):
+        con = sqlite3.connect("teachers.sqlite")
+        self.cursor = con.cursor()
 
 
-def fetchbydescription(desc):    #находит учителей по любому отрывку описания
-    global cursor
-    res = cursor.execute("""SELECT * FROM teachers WHERE description like ? """, ('%' + desc + '%',))
-    return list(res)
+    def insert(self, i, n, s, e, desc):  #функция, вносящая нового учителя в БД (имя, предмет, опыт работы, описание)
+        self.cursor.execute(f"INSERT INTO teachers(ind,name,subject,experience,description) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', s, '"'])}, {e}, {''.join(['"', desc, '"'])})")
 
+    def fetchbyindex(self, i):       #находит препода по индексу (они по порядку будут)
+        res = self.cursor.execute(f"SELECT * FROM teachers WHERE ind = {i}")
+        return list(res)
+
+    def fetchbyname(self, n):         #находит нужных учителей по любому отрывку имени
+        res = self.cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + n + '%',))
+        return list(res)
+
+    def fetchbysubject(self, s):       #находит нужных учителей по любой части названия предмета
+        res = self.cursor.execute("SELECT * FROM teachers WHERE subject like ?", ('%' + s + '%',))
+        return list(res)
+
+    def fetchbyexperience(self, e):    #находит нужных учителей по опыту работы (если опыт больше или равен нужному, выводит страничку препода)
+        res = self.cursor.execute(f"SELECT * FROM teachers WHERE experience >= {e}")
+        return list(res)
+
+
+    def fetchbydescription(self, desc):    #находит учителей по любому отрывку описания
+        res = self.cursor.execute("""SELECT * FROM teachers WHERE description like ? """, ('%' + desc + '%',))
+        return list(res)
 
 
 
 
-con = sqlite3.connect("teachers.sqlite")
-cursor = con.cursor()
-insert(1, "Anya", "maths", 23, "hello, my friends!")
-insert(2, "Tobey", "maths", 12, "yo bozo")
+
+base = BD()  #класс базы данных, ему можно дать любое имя, я выбрал base
+#base.insert(1, "Anya", "maths", 23, "hello, my friends!")
+#base.insert(2, "Tobey", "maths", 12, "yo bozo")
 #print(fetchbyindex(1))
 #print(fetchbydescription(""))
 #print(fetchbyname("y"))
