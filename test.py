e64267178fc484da293b078c82f620e12d436b80
nebugg <antonbegun@icloud.com>
2025-03-01 11:04:28 +0300

    test03

--- test.py
+++ test.py
@@ -6,20 +6,24 @@ class BD:
         self.cursor = con.cursor()
 
 
-    def insert(self, i, n, s, e, desc):  #функция, вносящая нового учителя в БД (имя, предмет, опыт работы, описание)
-        self.cursor.execute(f"INSERT INTO teachers(ind,name,subject,experience,description) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', s, '"'])}, {e}, {''.join(['"', desc, '"'])})")
+    def insert(self, i, n, c, s, e, desc):  #функция, вносящая нового учителя в БД (иднекс, имя, направление, предмет, опыт работы, описание)
+        self.cursor.execute(f"INSERT INTO teachers(ind,name,category,subject,experience,description) VALUES({i}, {''.join(['"', n, '"'])}, {''.join(['"', c, '"'])}, {''.join(['"', s, '"'])}, {e}, {''.join(['"', desc, '"'])})")
+
 
     def fetchbyindex(self, i):       #находит препода по индексу (они по порядку будут)
         res = self.cursor.execute(f"SELECT * FROM teachers WHERE ind = {i}")
         return list(res)
 
+
     def fetchbyname(self, n):         #находит нужных учителей по любому отрывку имени
         res = self.cursor.execute("SELECT * FROM teachers WHERE name like ? ", ('%' + n + '%',))
         return list(res)
 
+
     def fetchbysubject(self, s):       #находит нужных учителей по любой части названия предмета
         res = self.cursor.execute("SELECT * FROM teachers WHERE subject like ?", ('%' + s + '%',))
         return list(res)
+
 
     def fetchbyexperience(self, e):    #находит нужных учителей по опыту работы (если опыт больше или равен нужному, выводит страничку препода)
         res = self.cursor.execute(f"SELECT * FROM teachers WHERE experience >= {e}")
@@ -31,18 +35,25 @@ class BD:
         return list(res)
 
 
+    def fetchbycategory(self, cat):    #находит учителей по любому отрывку направления
+        res = self.cursor.execute("""SELECT * FROM teachers WHERE category like ? """, ('%' + cat + '%',))
+        return list(res)
+
+
+
 
 
 
 base = BD()  #класс базы данных, ему можно дать любое имя, я выбрал base
-#base.insert(1, "Anya", "maths", 23, "hello, my friends!")
-#base.insert(2, "Tobey", "maths", 12, "yo bozo")
-#print(fetchbyindex(1))
-#print(fetchbydescription(""))
-#print(fetchbyname("y"))
-#print(fetchbyexperience("22"))
-#print(fetchbyexperience("23"))
-#print(fetchbysubject("mat"))       # примеры для проверки работы кода (убери комменты и проверяй)
+base.insert(1, "Anya", "physics and mathematics", "maths", 23, "hello, my friends!")
+base.insert(2, "Tobey", "Internet Technology", "maths", 12, "yo bozo")
+#print(base.fetchbycategory("in"))
+#print(base.fetchbyindex(1))
+#print(base.fetchbydescription(""))
+#print(base.fetchbyname("y"))
+#print(base.fetchbyexperience("22"))
+#print(base.fetchbyexperience("23"))
+#print(base.fetchbysubject("mat"))       # примеры для проверки работы кода (убери комменты и проверяй)
 
 
 #есть вопросы - пишите в телегу
